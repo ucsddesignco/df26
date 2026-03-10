bd99f58b3033742dd073699f871c8ebaade0e873
pa1kina <p.volskaya@gmail.com>
2026-03-09 19:42:24 -0700

    merging faq and agenda

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Agenda from "./Components/Agenda/Agenda";
+import "./App.scss";
+import FAQ from './Components/FAQ'
 
-function App() {
-  return <Agenda />;
+
+  function App() {
+  return <>
+  <Agenda></Agenda>
+  <FAQ></FAQ>
+  </>;
 }
 
 export default App;

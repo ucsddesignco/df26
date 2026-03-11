61c334c2b57dcdc92659159e1b1c544901cf39ae
Shamita Goyal <goyal.shamita4@gmail.com>
2026-03-11 01:06:17 -0700

    deleted components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import Faq from "./Components/Faq/Faq"
-import Agenda from "./Components/AgendaSection/Agenda"
 
 function App() {
   return (
     <>
       <h1>hi this is the website</h1>
-      <Agenda />
-      <Faq />
     </>
   );
 }

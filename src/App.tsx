58dc65cc266c87ee80860e0043ba15464b76cdfa
Shamita Goyal <goyal.shamita4@gmail.com>
2026-04-01 16:01:46 -0700

    feat: added zach's footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import FAQ from "./Components/FAQ/FAQ"
 import Agenda from "./Components/Agenda/Agenda"
+import Footer from "./Components/Footer/Footer"
 function App() {
   return (
     <>
       <h1>hi this is the website</h1>
       <Agenda/>
       <FAQ/>
+      <Footer/>
     </>
   );
 }

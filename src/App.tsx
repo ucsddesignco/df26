c7a4a2f419f4e4fea8b0c33280940d039959220f
Shamita Goyal <goyal.shamita4@gmail.com>
2026-04-13 15:41:58 -0700

    feat: palina's faq flowers, hero section deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Hero from "./Components/Hero/Hero"
+
 import FAQ from "./Components/FAQ/FAQ"
 import Agenda from "./Components/Agenda/Agenda"
 import Footer from "./Components/Footer/Footer"
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
     <Navbar/>
-    <Hero />
     <Agenda/>
     <FAQ/>
     <Footer/>

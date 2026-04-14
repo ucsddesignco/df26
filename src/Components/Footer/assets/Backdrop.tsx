2b26a561d7b5f40b370a09c06a2e32a770c4f7c9
pa1kina <p.volskaya@gmail.com>
2026-04-13 20:54:42 -0700

    added grain to sunrise-sunset backdrop

--- src/Components/Footer/assets/Backdrop.tsx
+++ src/Components/Footer/assets/Backdrop.tsx
@@ -13,19 +13,18 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
       viewBox="0 0 1280 480"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      href="http://www.w3.org/1999/xlink"
     >
-      <g clipPath="url(#clip0_2497_10823)">
+      <g clip-path="url(#clip0_2493_10247)">
         <rect width="1280" height="480" fill="#F8CABA" />
-        <g filter="url(#filter0_ng_2497_10823)">
+        <g filter="url(#filter0_ng_2493_10247)">
           <path
             d="M1084.38 313C1097.9 313 1108.86 323.96 1108.86 337.48C1108.86 341.235 1108.01 344.793 1106.5 347.973H1240.88C1254.4 347.973 1265.36 358.933 1265.36 372.453C1265.36 385.973 1254.4 396.934 1240.88 396.935H1178.19C1179.71 400.114 1180.55 403.671 1180.55 407.426C1180.55 420.946 1169.59 431.907 1156.07 431.907H791.48C777.96 431.907 767 420.946 767 407.426C767 393.906 777.96 382.945 791.48 382.945H854.166C852.655 379.765 851.809 376.208 851.809 372.453C851.809 368.698 852.655 365.142 854.166 361.962H838.693C825.173 361.962 814.213 351.001 814.213 337.48C814.213 323.96 825.173 313 838.693 313H1084.38Z"
             fill="white"
           />
         </g>
         <mask
-          id="mask0_2497_10823"
-        //   style="mask-type:alpha"
+          id="mask0_2493_10247"
+          style={{maskType:'alpha'}}
           maskUnits="userSpaceOnUse"
           x="-142"
           y="-65"
@@ -37,10 +36,10 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             y="-65"
             width="1240"
             height="1240"
-            fill="url(#pattern0_2497_10823)"
+            fill="url(#pattern0_2493_10247)"
           />
         </mask>
-        <g mask="url(#mask0_2497_10823)">
+        <g mask="url(#mask0_2493_10247)">
           <rect
             x="-212.725"
             y="-204.251"
@@ -49,34 +48,34 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             fill="#E17490"
           />
         </g>
-        <g filter="url(#filter1_ng_2497_10823)">
+        <g filter="url(#filter1_ng_2493_10247)">
           <path
             d="M402.099 31C415.619 31.0002 426.579 41.9603 426.579 55.4805C426.579 62.3299 423.765 68.5216 419.231 72.9648H550.732C564.253 72.965 575.213 83.9252 575.213 97.4453C575.213 110.966 564.253 121.927 550.732 121.927H330.922C335.458 126.37 338.273 132.563 338.273 139.414C338.273 146.266 335.457 152.459 330.921 156.902H475.541C489.061 156.902 500.021 167.863 500.021 181.384C500.021 194.904 489.061 205.864 475.541 205.864H146.798C133.278 205.864 122.318 194.904 122.317 181.384C122.317 174.533 125.132 168.339 129.668 163.896H72.4805C58.9602 163.895 48 152.934 48 139.414C48.0002 125.894 58.9604 114.934 72.4805 114.934H153.275C148.739 110.49 145.924 104.297 145.924 97.4453C145.924 90.5962 148.737 84.4051 153.271 79.9619H107.453C93.9329 79.9617 82.9727 69.0008 82.9727 55.4805C82.9729 41.9604 93.933 31.0002 107.453 31H402.099Z"
             fill="white"
           />
         </g>
-        <g filter="url(#filter2_g_2497_10823)">
+        <g filter="url(#filter2_g_2493_10247)">
           <path
             d="M952.755 255.404C958.995 257.66 962.96 263.416 963.237 269.658C967.44 265.036 974.169 263.145 980.41 265.401C988.667 268.385 992.942 277.499 989.957 285.756C987.702 291.996 981.947 295.96 975.706 296.238C980.327 300.441 982.218 307.168 979.963 313.408C976.979 321.666 967.865 325.94 959.607 322.956C953.366 320.7 949.401 314.944 949.125 308.701C944.921 313.324 938.193 315.216 931.952 312.961C923.695 309.976 919.419 300.862 922.403 292.604C924.658 286.365 930.413 282.4 936.654 282.122C932.034 277.918 930.144 271.191 932.399 264.952C935.383 256.695 944.497 252.42 952.755 255.404ZM959.119 281.05C956.274 284.177 952.275 286.054 948.051 286.241C951.179 289.086 953.054 293.087 953.242 297.31C956.086 294.182 960.086 292.307 964.31 292.119C961.182 289.274 959.306 285.273 959.119 281.05Z"
             fill="#ED9699"
           />
         </g>
-        <g filter="url(#filter3_g_2497_10823)">
+        <g filter="url(#filter3_g_2493_10247)">
           <path
             d="M334.923 333.405C341.11 330.42 348.546 333.016 351.53 339.203C353.786 343.879 352.851 349.267 349.59 352.908C354.471 352.623 359.269 355.245 361.525 359.922C364.51 366.108 361.913 373.544 355.726 376.529C351.049 378.785 345.659 377.85 342.018 374.586C342.305 379.468 339.683 384.268 335.005 386.525C328.819 389.509 321.384 386.913 318.399 380.726C316.143 376.05 317.076 370.662 320.338 367.02C315.458 367.305 310.66 364.683 308.404 360.007C305.42 353.82 308.016 346.386 314.203 343.401C318.879 341.145 324.268 342.079 327.91 345.341C327.623 340.46 330.246 335.661 334.923 333.405ZM332.024 353.869C332.218 357.173 331.078 360.438 328.869 362.904C332.174 362.711 335.44 363.852 337.905 366.061C337.711 362.757 338.85 359.49 341.059 357.024C337.754 357.217 334.489 356.077 332.024 353.869Z"
             fill="#ED9699"
           />
         </g>
-        <g filter="url(#filter4_g_2497_10823)">
+        <g filter="url(#filter4_g_2493_10247)">
           <path
-            d="M825.143 168.905C831.233 169.725 835.506 175.327 834.686 181.418C834.066 186.023 830.711 189.586 826.48 190.696C830.267 192.887 832.56 197.212 831.939 201.817C831.119 207.907 825.516 212.18 819.426 211.36C814.821 210.739 811.257 207.384 810.146 203.152C807.955 206.938 803.632 209.229 799.028 208.609C792.938 207.789 788.665 202.186 789.485 196.096C790.105 191.493 793.457 187.93 797.686 186.817C793.902 184.625 791.612 180.303 792.232 175.701C793.053 169.61 798.656 165.337 804.746 166.158C809.35 166.778 812.912 170.131 814.024 174.361C816.215 170.576 820.539 168.285 825.143 168.905ZM812.893 182.757C811.41 185.32 808.95 187.196 806.087 187.949C808.65 189.433 810.526 191.894 811.278 194.759C812.762 192.196 815.223 190.319 818.088 189.566C815.524 188.083 813.646 185.622 812.893 182.757Z"
+            d="M825.142 168.905C831.233 169.725 835.506 175.327 834.686 181.418C834.065 186.023 830.711 189.586 826.479 190.696C830.266 192.887 832.559 197.212 831.939 201.817C831.119 207.907 825.516 212.18 819.425 211.36C814.82 210.739 811.256 207.384 810.146 203.152C807.955 206.938 803.632 209.229 799.028 208.609C792.937 207.789 788.665 202.186 789.485 196.096C790.105 191.493 793.457 187.93 797.685 186.817C793.902 184.625 791.612 180.303 792.232 175.701C793.052 169.61 798.655 165.337 804.746 166.158C809.349 166.778 812.912 170.131 814.024 174.361C816.215 170.576 820.539 168.285 825.142 168.905ZM812.893 182.757C811.409 185.32 808.95 187.196 806.086 187.949C808.649 189.433 810.525 191.894 811.277 194.759C812.761 192.196 815.223 190.319 818.088 189.566C815.524 188.083 813.646 185.622 812.893 182.757Z"
             fill="#ED9699"
           />
         </g>
       </g>
       <defs>
         <filter
-          id="filter0_ng_2497_10823"
+          id="filter0_ng_2493_10247"
           x="764.645"
           y="310.645"
           width="503.07"
@@ -123,7 +122,7 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             in="color1Flood"
             result="color1"
           />
-          <feMerge result="effect1_noise_2497_10823">
+          <feMerge result="effect1_noise_2493_10247">
             <feMergeNode in="shape" />
             <feMergeNode in="color1" />
           </feMerge>
@@ -134,7 +133,7 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             seed="5210"
           />
           <feDisplacementMap
-            in="effect1_noise_2497_10823"
+            in="effect1_noise_2493_10247"
             scale="4.7100076675415039"
             xChannelSelector="R"
             yChannelSelector="G"
@@ -142,20 +141,20 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             width="100%"
             height="100%"
           />
-          <feMerge result="effect2_texture_2497_10823">
+          <feMerge result="effect2_texture_2493_10247">
             <feMergeNode in="displacedImage" />
           </feMerge>
         </filter>
         <pattern
-          id="pattern0_2497_10823"
+          id="pattern0_2493_10247"
           patternContentUnits="objectBoundingBox"
           width="1"
           height="1"
         >
-          <use href="#image0_2497_10823" transform="scale(0.000488281)" />
+          <use href="#image0_2493_10247" transform="scale(0.000488281)" />
         </pattern>
         <filter
-          id="filter1_ng_2497_10823"
+          id="filter1_ng_2493_10247"
           x="45.645"
           y="28.645"
           width="531.923"
@@ -202,7 +201,7 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             in="color1Flood"
             result="color1"
           />
-          <feMerge result="effect1_noise_2497_10823">
+          <feMerge result="effect1_noise_2493_10247">
             <feMergeNode in="shape" />
             <feMergeNode in="color1" />
           </feMerge>
@@ -213,7 +212,7 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             seed="5210"
           />
           <feDisplacementMap
-            in="effect1_noise_2497_10823"
+            in="effect1_noise_2493_10247"
             scale="4.7100076675415039"
             xChannelSelector="R"
             yChannelSelector="G"
@@ -221,15 +220,15 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             width="100%"
             height="100%"
           />
-          <feMerge result="effect2_texture_2497_10823">
+          <feMerge result="effect2_texture_2493_10247">
             <feMergeNode in="displacedImage" />
           </feMerge>
         </filter>
         <filter
-          id="filter2_g_2497_10823"
-          x="919.539"
+          id="filter2_g_2493_10247"
+          x="919.54"
           y="252.541"
-          width="73.2817"
+          width="73.2813"
           height="73.2788"
           filterUnits="userSpaceOnUse"
           color-interpolation-filters="sRGB"
@@ -256,15 +255,15 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             width="100%"
             height="100%"
           />
-          <feMerge result="effect1_texture_2497_10823">
+          <feMerge result="effect1_texture_2493_10247">
             <feMergeNode in="displacedImage" />
           </feMerge>
         </filter>
         <filter
-          id="filter3_g_2497_10823"
+          id="filter3_g_2493_10247"
           x="305.67"
           y="330.67"
-          width="58.5896"
+          width="58.5901"
           height="58.5891"
           filterUnits="userSpaceOnUse"
           color-interpolation-filters="sRGB"
@@ -291,15 +290,15 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             width="100%"
             height="100%"
           />
-          <feMerge result="effect1_texture_2497_10823">
+          <feMerge result="effect1_texture_2493_10247">
             <feMergeNode in="displacedImage" />
           </feMerge>
         </filter>
         <filter
-          id="filter4_g_2497_10823"
-          x="788.046"
+          id="filter4_g_2493_10247"
+          x="788.045"
           y="164.718"
-          width="48.08"
+          width="48.0805"
           height="48.0809"
           filterUnits="userSpaceOnUse"
           color-interpolation-filters="sRGB"
@@ -326,15 +325,15 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
             width="100%"
             height="100%"
           />
-          <feMerge result="effect1_texture_2497_10823">
+          <feMerge result="effect1_texture_2493_10247">
             <feMergeNode in="displacedImage" />
           </feMerge>
         </filter>
-        <clipPath id="clip0_2497_10823">
+        <clipPath id="clip0_2493_10247">
           <rect width="1280" height="480" fill="white" />
         </clipPath>
         <image
-          id="image0_2497_10823"
+          id="image0_2493_10247"
           width="2048"
           height="2048"
           preserveAspectRatio="none"
@@ -361,7 +360,7 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
         </g>
         <mask
           id="mask0_2669_12754"
-          style={{ maskType: 'alpha' }}
+          style={{ maskType: "alpha" }}
           maskUnits="userSpaceOnUse"
           x="-142"
           y="-65"
@@ -779,7 +778,7 @@ const ThemeBackdrops: Record<ThemeType, React.ReactNode> = {
         </g>
         <mask
           id="mask0_2685_14847"
-          style= {{maskType: 'alpha'}}
+          style={{ maskType: "alpha" }}
           maskUnits="userSpaceOnUse"
           x="-142"
           y="-65"

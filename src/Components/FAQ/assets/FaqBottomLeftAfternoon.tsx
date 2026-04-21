9ae2a9a8b3c1fd8f5f25f546fe0805b4acbf6d36
pa1kina <p.volskaya@gmail.com>
2026-04-21 00:32:11 -0700

    custom faq sticker positions for different time of day variants

---
+++ src/Components/FAQ/assets/FaqBottomLeftAfternoon.tsx
@@ -0,0 +1,98 @@
+export const FaqBottomLeftAfternoon = () => {
+  return (
+    <div>
+      <svg
+        width="133"
+        height="130"
+        viewBox="0 0 133 130"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_g_2657_11510)">
+          <path
+            d="M87.9909 23.9023C95.1063 32.3577 95.0882 44.0814 87.9505 50.0879C80.8128 56.0944 69.2584 54.1091 62.143 45.6537C58.0571 40.7983 56.128 29.8554 55.2261 21.4175C54.6158 15.7082 59.7717 11.3694 65.2928 12.9462C73.4526 15.2765 83.9049 19.0468 87.9909 23.9023Z"
+            fill="#AEB032"
+          />
+        </g>
+        <g filter="url(#filter1_g_2657_11510)">
+          <path
+            d="M45.7563 76.1652C53.2666 76.6225 59.6679 71.8538 60.0539 65.5139C60.44 59.174 54.6646 53.6638 47.1543 53.2064C43.4014 52.9779 37.7918 55.5133 33.1181 58.1074C28.6555 60.5844 28.2894 66.5972 32.4184 69.5973C36.7428 72.7394 42.0034 75.9367 45.7563 76.1652Z"
+            fill="#AEB032"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_g_2657_11510"
+            x="53.6768"
+            y="11.1193"
+            width="41.1387"
+            height="43.9399"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.125 0.125"
+              numOctaves="3"
+              seed="2758"
+            />
+            <feDisplacementMap
+              in="shape"
+              scale="3"
+              xChannelSelector="R"
+              yChannelSelector="G"
+              result="displacedImage"
+              width="100%"
+              height="100%"
+            />
+            <feMerge result="effect1_texture_2657_11510">
+              <feMergeNode in="displacedImage" />
+            </feMerge>
+          </filter>
+          <filter
+            id="filter1_g_2657_11510"
+            x="28.0342"
+            y="51.6921"
+            width="33.5381"
+            height="26.0034"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.125 0.125"
+              numOctaves="3"
+              seed="2758"
+            />
+            <feDisplacementMap
+              in="shape"
+              scale="3"
+              xChannelSelector="R"
+              yChannelSelector="G"
+              result="displacedImage"
+              width="100%"
+              height="100%"
+            />
+            <feMerge result="effect1_texture_2657_11510">
+              <feMergeNode in="displacedImage" />
+            </feMerge>
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+};

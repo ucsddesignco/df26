ff1ce0bbe4b854148538a3238332b30064a092c7
Kirsten Arcinas <kirsten@kirstens-air.lan>
2026-04-20 20:51:13 -0700

    new additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-
 import FAQ from "./Components/FAQ/FAQ"
 import Agenda from "./Components/Agenda/Agenda"
 import Footer from "./Components/Footer/Footer"
+import WhatDesignFrontiers from "./Components/WhatIs/WhatDesignFrontiers"
 import { Navbar } from "./Components/Navbar/Navbar"
 
 function App() {
   return (
     <>
     <Navbar/>
+    <WhatDesignFrontiers/>
     <Agenda/>
     <FAQ/>
     <Footer/>
